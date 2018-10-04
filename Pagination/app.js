a03313570662cdfdbd8ac7f6c76ebe9d16f62db6
WIN-6NL2QLBEESP\hyoni <dudu1104@naver.com>
2018-10-04 16:22:20 +0900

    서버 구동 관련 소스

---
+++ Pagination/app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const morgan = require('morgan');
+const bodyParser = require('body-parser'); //이걸 써야 body값을 받는다.
+const app = express();
+const reviewRouter = require('./router/reviewRouter.js');
+
+//이걸 써야 body값을 받는다.
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json()); /
+
+app.use(morgan('dev'));
+app.use(reviewRouter);
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+
+app.use(express.static('public'));
+
+
+app.use((err, req, res, next) => {
+  res.send({error: err.message});
+})
+
+app.listen(3000,function () {
+  console.log('success');
+});
