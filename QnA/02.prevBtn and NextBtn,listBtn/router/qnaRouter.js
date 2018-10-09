12739c412e878012a7a9d5e3c3599f5780fedcd3
WIN-6NL2QLBEESP\hyoni <dudu1104@naver.com>
2018-10-09 19:41:53 +0900

    QnA 글 목록 및 글보기 관련 라우팅

---
+++ QnA/02.prevBtn and NextBtn,listBtn/router/qnaRouter.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const pool = require('../model/dbConnection.js');
+const QnA = require('../model/qna.js');
+
+const router = express.Router();
+
+router.route('/qna')
+  .get(showQnA);
+
+
+router.route('/qnaView')
+  .get(showQnAWri);
+
+
+function showQnA(req, res) {
+  QnA.getQnAList( req.query,(err, result) => {
+    if(err) {
+      res.status(500).send({msg:'getQnAList error'});
+      return;
+    }
+      res.render('qna.ejs', result);
+  }
+)}
+
+function showQnAWri(req, res) {
+  const wriQnANo = req.query.wriNo;
+  const pageNum = req.query.page;
+  const inputSuccess = 'N';
+
+  QnA.getQnAWri( req.query, inputSuccess, (err, result) => {
+    if(err) {
+      res.status(500).send({msg:'getQnAWri error'});
+      return;
+    }// 비밀글인거 체크하고 보여주기
+      res.render('qnaWri.ejs', result);
+  })
+}
+
+
+
+module.exports = router;
