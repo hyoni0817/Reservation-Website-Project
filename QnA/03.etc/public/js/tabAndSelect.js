a7d6cc7d1204c660588a526cda0f7bdaf41e8403
WIN-6NL2QLBEESP\hyoni <dudu1104@naver.com>
2018-10-11 00:15:25 +0900

    카테고리 탭과 질문 답변 여부 선택해서 검색하는 관련 소스코드

---
+++ QnA/03.etc/public/js/tabAndSelect.js
@@ -0,0 +1,31 @@
+//QnA 글 목록에서 카테고리 선택하는 탭
+function inquiryNavTab(inquiryNum, contents, ansNum, condiNum) {
+  var element = document.getElementsByClassName("nav-inquiry");
+
+  var navHTML = '<ul class="nav nav-pills">';
+
+  for(var i = 0; i < 5; i++) {
+    navHTML += '<li class="nav-item"><a class="nav-link '+(inquiryNum == i ? 'active' : '')+'" href="/qna?inquiry=' +
+    i +'&searchContent='+contents+'&answer='+ansNum+'&condition='+condiNum+ '">';
+    if(i===0){
+      navHTML += '전체'
+    } else if(i===1) {
+      navHTML += '예약 문의'
+    } else if(i===2) {
+      navHTML += '운영 문의'
+    } else if(i===3) {
+      navHTML += '기타 문의'
+    } else if(i===4) {
+      navHTML += '건의 사항'
+    }
+    navHTML += '</a></li>';
+  }
+ element[0].innerHTML = navHTML;
+}
+
+//질문전체 답변 완료 선택하는 부분
+function answerSel(inquiryNum, contents, ansNum, condiNum) {
+  var ansEle = document.getElementById("answer");
+
+  location.href='/qna?inquiry='+inquiryNum+'&answer='+ansEle.value+'&searchContent='+contents+'&condition='+condiNum;
+}
