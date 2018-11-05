4a2ed11880e3dc034d73f8597f05ad95fdb7fc26
WIN-6NL2QLBEESP\hyoni <dudu1104@naver.com>
2018-11-05 23:30:36 +0900

    서버 구동 관련 소스코드

---
+++ Review/app.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const app = express();
+const reviewRouter = require('./router/reviewRouter.js');
+
+// Ajax 관련 헤더를 추가합니다.
+//다음을 추가 안하면 ajax node.js Response to preflight request doesn't pass access control check 와 같은 오류 발생
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.use(morgan('dev'));
+app.use(reviewRouter);
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+
+app.use(express.static('public'));
+
+
+app.use((err, req, res, next) => {
+  res.send({error: err.message});
+})
+
+app.listen(3000,function () {
+  console.log('success');
+});
